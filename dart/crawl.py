78762796885ad284d880180aa873776555d0f185
monbijou3 <95562885+monbijou3@users.noreply.github.com>
2024-02-06 19:19:55 +0900

    Add files via upload

---
+++ dart/crawl.py
@@ -0,0 +1,78 @@
+import os
+import sys
+import urllib.request
+import datetime
+import time
+import json
+import boto3
+import pandas as pd
+import s3
+
+
+client_id = '9pSa1C5Lj0Yy8_Pd3mn3'
+client_secret = '88ZTtceaKm'
+#[CODE 1]
+def getRequestUrl(url):
+    req = urllib.request.Request(url)
+    req.add_header("X-Naver-Client-Id", client_id)
+    req.add_header("X-Naver-Client-Secret", client_secret)
+    try:
+        response = urllib.request.urlopen(req)
+        if response.getcode() == 200:
+            print("[%s]Url Request Success" % datetime.datetime.now())
+            return response.read().decode('utf-8')
+    except Exception as e :
+        print(e)
+        print("[%s] Error for URL : %s" % (datetime.datetime.now(), url))
+        return None
+#[CODE 2]
+def getNaverSearch(node, srcText, start, display):
+    base = "https://openapi.naver.com/v1/search"
+    node = "/%s.json" % node
+    parameters = "?query=%s&start=%s&display=%s" % (urllib.parse.quote(srcText), start, display)
+    url = base + node + parameters
+    responseDecode = getRequestUrl(url)     #[CODE 1]
+    if(responseDecode == None):
+        return None
+    else:
+        return json.loads(responseDecode)
+#[CODE 3]
+def getPostData(post, jsonResult, cnt):
+    title = post['title']
+    jsonResult.append({'cnt':cnt, 'title':title})
+    return
+#[CODE 0]
+def main():
+    node = 'news'  #크롤링한 대상
+    srcText = input('검색어를 입력하세요: ')
+    cnt = 0
+    jsonResult = []
+    jsonResponse = getNaverSearch(node, srcText, 1, 100)  #[CODE 2]
+    total = jsonResponse['total']
+    while((jsonResponse != None) and (jsonResponse['display'] != 0)):
+        for post in jsonResponse['items']:
+            cnt+= 1
+            getPostData(post, jsonResult, cnt)   #[CODE 3]
+        start = jsonResponse['start'] + jsonResponse['display']
+        jsonResponse = getNaverSearch(node, srcText, start, 100)   #[CODE 2]
+    print('전체 검색 : %d 건' %total)
+    with open('%s_naver_%s.json' % (srcText, node), 'w', encoding='utf8') as outfile:
+        jsonFile = json.dumps(jsonResult, indent=4, sort_keys = True, ensure_ascii = False)
+        outfile.write(jsonFile)
+    print("가져온 데이터 : %d 건" %(cnt))
+    print('%s_naver_%s.json SAVED' % (srcText, node))
+    s3.upload_json()
+
+    
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
+
+
+
+
